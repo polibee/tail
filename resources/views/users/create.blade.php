6e535d7297dcd7c2e998916fa7a98acb68c411a5
polibee <1244762327@qq.com>
2023-12-18 09:44:35 +0800

    user signup form

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -1,5 +1,36 @@
 @extends('layouts.default')
-@section('title','注册')
+
+@section('title', '注册')
+
 @section('content')
-    <h1>注册</h1>
+    <div class="mx-auto mt-8 max-w-md">
+        <div class="bg-white p-8 border border-gray-300 rounded shadow-md">
+            <h5 class="text-2xl font-semibold mb-4">注册</h5>
+            <form method="POST" action="{{ route('users.store') }}">
+                @csrf
+
+                <div class="mb-4">
+                    <label for="name" class="block text-sm font-medium text-gray-600">名称：</label>
+                    <input type="text" name="name" id="name" class="mt-1 p-2 w-full border rounded-md">
+                </div>
+
+                <div class="mb-4">
+                    <label for="email" class="block text-sm font-medium text-gray-600">邮箱：</label>
+                    <input type="text" name="email" id="email" class="mt-1 p-2 w-full border rounded-md">
+                </div>
+
+                <div class="mb-4">
+                    <label for="password" class="block text-sm font-medium text-gray-600">密码：</label>
+                    <input type="password" name="password" id="password" class="mt-1 p-2 w-full border rounded-md">
+                </div>
+
+                <div class="mb-6">
+                    <label for="password_confirmation" class="block text-sm font-medium text-gray-600">确认密码：</label>
+                    <input type="password" name="password_confirmation" id="password_confirmation" class="mt-1 p-2 w-full border rounded-md">
+                </div>
+
+                <button type="submit" class="w-full bg-blue-500 text-white p-2 rounded-md">注册</button>
+            </form>
+        </div>
+    </div>
 @stop
