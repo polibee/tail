02a08c75fcb8369c2edb04f9d42afda6964b8202
polibee <1244762327@qq.com>
2023-12-18 14:18:48 +0800

    error message

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -3,8 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite('resources/css/app.css')
     <title>@yield('title','Weibo')-Laravel新手项目</title>
-    <script src="https://cdn.tailwindcss.com"></script>
+
 </head>
 <body class="bg-gray-100">
 @include('layouts._header')
--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -2,36 +2,37 @@
 @section('title', '注册')
 
 @section('content')
-    <div class="offset-md-2 col-md-8">
-        <div class="card ">
-            <div class="card-header">
-                <h5>注册</h5>
-            </div>
-            <div class="card-body">
+    <div class="flex justify-center">
+        <div class="w-1/2">
+            <div class="bg-white rounded-lg shadow-md p-6">
+                <h5 class="text-lg font-bold mb-4">注册</h5>
+
+                @include('shared._errors')
+
                 <form method="POST" action="{{ route('users.store') }}">
                     {{ csrf_field() }}
 
-                    <div class="mb-3">
-                        <label for="name">名称：</label>
-                        <input type="text" name="name" class="form-control" value="{{ old('name') }}">
+                    <div class="mb-4">
+                        <label for="name" class="block mb-2">名称：</label>
+                        <input type="text" name="name" class="border border-gray-300 rounded-md px-3 py-2 w-full" value="{{ old('name') }}">
                     </div>
 
-                    <div class="mb-3">
-                        <label for="email">邮箱：</label>
-                        <input type="text" name="email" class="form-control" value="{{ old('email') }}">
+                    <div class="mb-4">
+                        <label for="email" class="block mb-2">邮箱：</label>
+                        <input type="text" name="email" class="border border-gray-300 rounded-md px-3 py-2 w-full" value="{{ old('email') }}">
                     </div>
 
-                    <div class="mb-3">
-                        <label for="password">密码：</label>
-                        <input type="password" name="password" class="form-control" value="{{ old('password') }}">
+                    <div class="mb-4">
+                        <label for="password" class="block mb-2">密码：</label>
+                        <input type="password" name="password" class="border border-gray-300 rounded-md px-3 py-2 w-full" value="{{ old('password') }}">
                     </div>
 
-                    <div class="mb-3">
-                        <label for="password_confirmation">确认密码：</label>
-                        <input type="password" name="password_confirmation" class="form-control" value="{{ old('password_confirmation') }}">
+                    <div class="mb-4">
+                        <label for="password_confirmation" class="block mb-2">确认密码：</label>
+                        <input type="password" name="password_confirmation" class="border border-gray-300 rounded-md px-3 py-2 w-full" value="{{ old('password_confirmation') }}">
                     </div>
 
-                    <button type="submit" class="btn btn-primary">注册</button>
+                    <button type="submit" class="bg-blue-500 text-white px-4 py-2 rounded-md">注册</button>
                 </form>
             </div>
         </div>
