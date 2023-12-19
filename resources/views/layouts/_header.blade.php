01b8f952b7fdfa805053157a47ffe325b13bfe8f
polibee <1244762327@qq.com>
2023-12-19 10:42:43 +0800

    login redirect

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -1,9 +1,42 @@
-<nav class="bg-gray-800">
-    <div class="container mx-auto">
-        <a class="text-white text-lg front-bold py-4" href="{{'home'}}">Weibo App</a>
-        <ul class="flex justify-end items-center">
-            <li class="mr-6"><a class="text-white" href="{{'help'}}">帮助</a></li>
-            <li><a class="text-white" href="#">登录</a></li>
-        </ul>
+<div class="navbar bg-base-100">
+    <div class="flex-1">
+        <a class="btn btn-ghost text-xl">Weibo App</a>
     </div>
-</nav>
+    <div class="flex-none gap-2">
+        <div class="form-control">
+            <input type="text" placeholder="Search" class="input input-bordered w-24 md:w-auto" />
+        </div>
+        @if (Auth::check())
+            <a class="btn btn-ghost" href="#">用户列表</a>
+
+        <div class="dropdown dropdown-end">
+            <div tabindex="0" role="button" class="btn btn-ghost btn-circle avatar">
+                <div class="w-10 rounded-full">
+                    <img alt="User Avatar" src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+                </div>
+            </div>
+            <ul tabindex="0" class="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
+                <li>
+                    <a class="justify-between">
+                        {{ Auth::user()->name }}
+                        <span class="badge">New</span>
+                    </a>
+                </li>
+                <li><a href="{{ route('users.show', Auth::user()) }}">个人中心</a></li>
+                <li><a href="#">编辑资料</a></li>
+                <li>
+                    <a href="{{ route('logout') }}"
+                       onclick="event.preventDefault(); document.getElementById('logout-form').submit();"
+                    >
+                        退出
+                    </a>
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                        @csrf
+                        @method('DELETE')
+                    </form>
+                </li>
+            </ul>
+        </div>
+        @endif
+    </div>
+</div>
