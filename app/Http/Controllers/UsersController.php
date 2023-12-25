8bff6c32dea7407abe83258c536628e47b1d764c
polibee <1244762327@qq.com>
2023-12-25 19:28:58 +0800

    remove from option

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -87,8 +87,8 @@ class UsersController extends Controller
         $name='coinowo';
         $to=$user->email;
         $subject='感谢注册weibo应用，请确认你的邮箱！';
-        Mail::send($view,$data,function($message) use ($from,$name,$to,$subject){
-            $message->from($from,$name)->to($to)->subject($subject);
+        Mail::send($view,$data,function($message) use ($to,$subject){
+            $message->to($to)->subject($subject);
         });
     }
     public function confirmEmail($token)
