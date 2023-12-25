fff4f12006d0cf30a76df704cf07878669e114a9
polibee <1244762327@qq.com>
2023-12-25 22:54:55 +0800

    delete tweet

---
+++ app/Policies/StatusPolicy.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+use App\Models\Status;
+class StatusPolicy
+{
+    /**
+     * Create a new policy instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+    public function destroy(User $user,Status $status)
+    {
+        return $user->id ===$status->user_id;
+    }
+}
--- resources/views/statuses/_statuses.blade.php
+++ resources/views/statuses/_statuses.blade.php
@@ -1,9 +1,17 @@
 <li class="flex mt-4 mb-4">
-    <a href="{{ route('users.show', $user->id )}}" class="flex-shrink-0">
-        <img src="{{ $user->gravatar() }}" alt="{{ $user->name }}" class="me-1 gravatar"/>
+    <a href="{{ route('users.show', $user->id) }}" class="flex-shrink-0">
+        <img src="{{ $user->gravatar() }}" alt="{{ $user->name }}" class="me-1 gravatar" />
     </a>
     <div class="flex-grow-1 ms-3">
-        <h5 class="mt-0 mb-1">{{ $user->name }} <small> / {{ $status->created_at->diffForHumans() }}</small></h5>
-        {{ $status->content }}
+        <h5 class="mt-0 mb-1 text-sm font-bold">{{ $user->name }} <small class="text-gray-500"> / {{ $status->created_at->diffForHumans() }}</small></h5>
+        <p class="text-gray-700">{{ $status->content }}</p>
     </div>
+
+    @can('destroy', $status)
+        <form action="{{ route('statuses.destroy', $status->id) }}" method="POST" class="ml-auto">
+            @csrf
+            @method('DELETE')
+            <button type="submit" class="btn btn-sm btn-red status-delete-btn">删除</button>
+        </form>
+    @endcan
 </li>
